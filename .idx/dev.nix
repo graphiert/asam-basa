b6a913801f47aa81ba94897cf0169d2d0b3ccbae
galihpujiirianto <galihpujiirianto@gmail.com>
2025-09-15 05:57:40 +0000

    Initial commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -23,16 +23,16 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
+        web = {
         #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
         #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0" ];
+          manager = "web";
+          env = {
+            # Environment variables to set for your server
+            PORT = "$PORT";
+          };
+        };
       };
     };
     # Workspace lifecycle hooks
